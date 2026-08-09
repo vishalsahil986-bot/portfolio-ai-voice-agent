00e50ac70ea81e4c608b64ec7e20401b9df13582
vishalsahilai <vishalsahilofficial@gmail.com>
2026-08-09 11:39:44 -0700

    feat: add CallState enum with LISTENING, THINKING, SPEAKING states

--- call/call_state_machine.py
+++ call/call_state_machine.py
@@ -3,3 +3,8 @@ from enum import Enum
 from utils.logger import get_logger
 
 logger = get_logger(__name__)
+
+class CallState(str, Enum):
+    LISTENING = "LISTENING"   # waiting for / receiving user audio
+    THINKING = "THINKING"     # STT done, waiting on LLM (+ RAG/memory) response
+    SPEAKING = "SPEAKING"     # streaming TTS audio back to the user
